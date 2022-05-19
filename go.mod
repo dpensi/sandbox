87715fe54c2bba8d6ff60b1bcd6e12a4fc89a7d9
Daniele Pensiero <daniele.pensiero@gmail.com>
2022-05-19 15:27:58 +0200

    updated

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.17
 require (
 	github.com/pelletier/go-toml v1.9.5
 	github.com/pelletier/go-toml/v2 v2.0.1
+	gopkg.in/yaml.v3 v3.0.0-20220512140231-539c8e751b99
 )
--- main.go
+++ main.go
@@ -1,62 +1,64 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	toml "github.com/pelletier/go-toml"
+	yaml "gopkg.in/yaml.v3"
 )
 
 type configuration struct {
-	url        string               `toml:"superset_url"`
-	path       string               `toml:"superset_dashboards_path"`
-	separator  string               `toml:"param_values_separator"`
-	dashboards map[string]dashboard `toml:"dashboards"`
+	Url        string               `json:"superset_url"`
+	Path       string               `json:"superset_dashboards_path"`
+	Separator  string               `json:"param_values_separator"`
+	Dashboards map[string]dashboard `json:"dashboards"`
 }
 
 type dashboard struct {
-	format string            `toml:"type"`
-	fields map[string]fields `toml:"fields"`
+	Format string            `json:"type"`
+	Fields map[string]fields `json:"fields"`
 }
 
 type fields struct {
-	id         string `toml:"id"`
-	columnName string `toml:"actual_column_name"`
-	def        string `toml:"default"`
+	Id         string `json:"id"`
+	ColumnName string `json:"actual_column_name"`
+	Def        string `json:"default"`
 }
 
 func main() {
 	fmt.Println("starting")
 
 	field_one := fields{
-		id:         "f1id",
-		columnName: "col1 name",
-		def:        "default",
+		Id:         "f1id",
+		ColumnName: "col1 name",
+		Def:        "default",
 	}
 	field_two := fields{
-		id:         "f2id",
-		columnName: "col2name",
-		def:        "default",
+		Id:         "f2id",
+		ColumnName: "col2name",
+		Def:        "default",
 	}
 	fields_map := make(map[string]fields)
 	fields_map["one"] = field_one
 	fields_map["two"] = field_two
 
 	dash_one := dashboard{
-		format: "one",
-		fields: fields_map,
+		Format: "one",
+		Fields: fields_map,
 	}
 	dash_two := dashboard{
-		format: "two",
-		fields: fields_map,
+		Format: "two",
+		Fields: fields_map,
 	}
 	dash_map := make(map[string]dashboard)
 	dash_map["unos"] = dash_one
 	dash_map["dos"] = dash_two
 	config := configuration{
-		url:        "uurrll",
-		path:       "ppaatthh",
-		separator:  ",",
-		dashboards: dash_map,
+		Url:        "uurrll",
+		Path:       "ppaatthh",
+		Separator:  ",",
+		Dashboards: dash_map,
 	}
 
 	b, err := toml.Marshal(config)
@@ -65,6 +67,18 @@ func main() {
 	}
 	fmt.Println(string(b))
 
-	fmt.Println("finished")
+	fmt.Println("finished toml")
+
+	d, err := yaml.Marshal(config)
+	if err != nil {
+		panic("oh no")
+	}
+	fmt.Printf("yaml:\n%s\n\n", string(d))
+
+	j, err := json.Marshal(config)
+	if err != nil {
+		panic("oh no")
+	}
+	fmt.Printf("json:\n%s\n\n", string(j))
 
 }
